52816a7c7a9839448d684f904b03b01962bf0b47
Bryan Manalo <bri.manalo@gmail.com>
2022-04-23 20:04:50 +0800

    Move to quasar.

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="row">
+      <q-card class="my-card col">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card col">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
